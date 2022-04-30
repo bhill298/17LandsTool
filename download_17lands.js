b9169e90807c56a989f3aa28e3ffd433cb5d4bb2
Brennan Hill <bhill298@gmail.com>
2022-04-29 20:23:34 -0600

    Add arg to get 17Lands data for only one set

--- download_17lands.js
+++ download_17lands.js
@@ -4,7 +4,7 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function the_thing() {
+async function the_thing(arguments) {
     var selection;
     var rows;
     var col_map = {};
@@ -12,12 +12,18 @@ async function the_thing() {
     var wr;
 
     var full_results = [];
-    var max_set_index = $("#expansion")[0].length;
+    var expansion_dropdown = $("#expansion")[0];
+    var max_set_index = expansion_dropdown.length;
     for (var set_index = 0; set_index < max_set_index; set_index++) {
         rows = undefined;
         results = {};
-        $("#expansion")[0].selectedIndex = set_index;
-        $("#expansion")[0].dispatchEvent(new Event('change', { bubbles: true }));
+        expansion_dropdown.selectedIndex = set_index;
+        if (arguments[0] == null || arguments[0].toUpperCase() === expansion_dropdown.value.toUpperCase()) {
+            expansion_dropdown.dispatchEvent(new Event('change', { bubbles: true }));
+        }
+        else {
+            continue;
+        }
         await sleep(1000);
         while ($('div.text-light')[0] === undefined || $('div.text-light')[0].innerText.search("don't have any") === -1) {
             selection = $("table.compact.table")[0];
@@ -44,10 +50,10 @@ async function the_thing() {
         }
         results_json = JSON.stringify(results)
         console.log(results_json);
-        file_name = $("#expansion")[0].value + "_17lands.json"
+        file_name = expansion_dropdown.value + "_17lands.json"
         console.log(file_name);
         full_results.push([results_json, file_name]);
     }
     return full_results;
 }
-return the_thing();
+return the_thing(arguments);
--- get_17lands_files.py
+++ get_17lands_files.py
@@ -1,20 +1,31 @@
+import argparse
 import time
 import json
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 
+parser = argparse.ArgumentParser(description="Download 17Lands json files")
+parser.add_argument('-s', '--set', default=None,
+    help="Download logs for one set, passing in expansion code (e.g. SNC, NEO, VOW). If not provided, download logs for all sets.")
+
+args = parser.parse_args()
+
 # Need google chrome installed, and webdriver extrected to a dir in system path (https://chromedriver.chromium.org/)
 driver = webdriver.Chrome()
 driver.minimize_window()
-driver.get('https://www.17lands.com/card_ratings')
+driver.get("https://www.17lands.com/card_ratings")
 time.sleep(1)
 script_contents = ''
 with open("download_17lands.js", 'r') as f:
     script_contents = f.read()
 driver.set_script_timeout(300)
+script_args = []
+script_args.append(args.set)
 try:
-    res = driver.execute_script(script_contents)
+    res = driver.execute_script(script_contents, *script_args)
+    if res is None:
+        raise RuntimeError("Script failed to return a value")
     for results, filename in res:
         # load and then dump again to make sure this is valid json
         results = json.loads(results)
